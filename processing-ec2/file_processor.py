c02975ed6a2254d1907c268438382ef30bdf48f7
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-09-29 08:57:01 +0000

    split class defs to another file

--- processing-ec2/file_processor.py
+++ processing-ec2/file_processor.py
@@ -2,48 +2,8 @@
 
 import hashlib
 import pefile
-from pydantic import BaseModel
 from utils import *
-
-# custom
-class SectionHash(BaseModel):
-    md5: str
-    sha1: str
-    sha256: str
-    ssdeep: str
-
-class SectionFileType(BaseModel):
-    python_magic: str
-    trid: list[str]
-
-class SectionMeta(BaseModel):
-    size: int
-    architecture: str
-    date: str
-    crc_claim: int | str
-    crc_calcd: int
-
-class SectionPESection(BaseModel):
-    name: str
-    virt_addr: int
-    virt_size: int
-    phys_size: int
-    md5: str
-    entropy: float
-    suspicious: bool
-
-class StaticReport(BaseModel):
-    hashes: SectionHash
-    file_type: SectionFileType
-    meta: SectionMeta
-    pe_sections: list[SectionPESection]
-
-# for disabling editor error
-# (doesn't work)
-# class Structure(pefile.Structure):
-#     Machine: str = ''
-#     TimeDateStamp: str = ''
-#     CheckSum: str = ''
+from section_types import *
 
 
 def get_pe_section_info(section) -> SectionPESection:
