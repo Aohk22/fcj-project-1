f844db044a0311e7b68adbff848569ecf9c99fa9
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-10-01 10:19:15 +0700

    Revert "Update utils.py"
    This reverts commit 3afaeb2b7989465070c8bfc7a6f0f2c55a2e01c1.

--- processing-ec2/helper/utils.py
+++ processing-ec2/helper/utils.py
@@ -1,73 +1,54 @@
 import subprocess as sp
+import magic
 import time
-
-try:
-    import magic
-except ImportError:
-    magic = None
-
-try:
-    import ssdeep  
-except ImportError:
-    ssdeep = None
-
 from trid.trid import tridAnalyze, trdpkg2defs
 
 
-PATH_TRIDDEF = "./triddefs.trd"
+PATH_TRIDDEF = './triddefs.trd'
 TRID_RESULT_CAP = 4
 
 
-def ssdeep_hash(raw_data: bytes) -> str:
-    if not ssdeep:
-        return "ssdeep not available"
-    try:
-        return ssdeep.hash(raw_data)
-    except Exception as e:
-        return f"ssdeep error: {type(e).__name__}"
+def ssdeep(raw_data: bytes) -> str:
+    output = sp.run(['ssdeep'], input=raw_data, capture_output=True).stdout
+    hash_result = output.split(b'\n')[1].split(b',')[0]
+    return hash_result.decode()
 
 
 def from_magic(raw_data: bytes) -> str:
-    if not magic:
-        return "libmagic not available"
-    try:
-        return magic.from_buffer(raw_data)
-    except Exception as e:
-        return f"magic error: {type(e).__name__}"
+    result = magic.from_buffer(raw_data)
+    return result
 
 
 def trid(raw_data: bytes) -> list[str]:
-    results_strs = []
-    try:
-        triddefs = trdpkg2defs(PATH_TRIDDEF, usecache=True)
-        results = tridAnalyze(raw_data, triddefs, stringcheck=True)
-        if results:
-            for res in results[:min(len(results), TRID_RESULT_CAP)]:
-                results_strs.append(
-                    f"{res.perc:5.1f}% (.{res.triddef.ext}) {res.triddef.filetype} ({res.pts}/{res.patt}/{res.str})"
-                )
-    except Exception as e:
-        results_strs.append(f"TrID error: {type(e).__name__}")
+    results_strs = list()
+    triddefs = trdpkg2defs(PATH_TRIDDEF, usecache=True)
+    results = tridAnalyze(raw_data, triddefs, stringcheck=True)
+    if results:
+        for res in results[:min(len(results), TRID_RESULT_CAP)]:
+            results_strs.append(("%5.1f%% (.%s) %s (%i/%i/%i)" % (
+                res.perc, res.triddef.ext,
+                res.triddef.filetype,
+                res.pts, res.patt, res.str)))
     return results_strs
 
 
 def get_arch(arch_num: int) -> str:
-    if arch_num == 0x14C:
-        return "32 bits"
-    elif arch_num == 0x8664:
-        return "64 bits"
+    if arch_num == 0x14c:
+        return '32 bits'
+    elif arch_num == 8664:
+        return '64 bits'
     else:
-        return "unknown"
+        return 'unknown'
 
 
 def get_timestamp(ts_nu: int) -> str:
-    ts = f"0x{ts_nu:08X}"
+    ts = '0x%-8X' % (ts_nu)
     try:
-        ts += f" [{time.asctime(time.gmtime(ts_nu))} UTC]"
+        ts += ' [%s UTC]' % time.asctime(time.gmtime(ts_nu))
         that_year = time.gmtime(ts_nu)[0]
         this_year = time.gmtime(time.time())[0]
         if that_year < 2000 or that_year > this_year:
-            ts += " [SUSPICIOUS]"
-    except Exception:
-        ts += " [SUSPICIOUS]"
+            ts += ' [SUSPICIOUS]'
+    except:
+        ts += ' [SUSPICIOUS]'
     return ts
