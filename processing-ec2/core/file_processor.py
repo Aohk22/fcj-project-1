1c35c0b90075fe96f04a17f471ed48978bacfabf
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-10-01 10:19:16 +0700

    Revert "Update file_processor.py"
    This reverts commit 5fcc531f744c56bbbd12c904f01297cdf21ed7ec.

--- processing-ec2/core/file_processor.py
+++ processing-ec2/core/file_processor.py
@@ -6,45 +6,47 @@ from helper import utils
 
 def get_pe_section_info(section) -> ctypes.SectionPESection:
     entr = section.get_entropy()
-    return ctypes.SectionPESection(
-        name=section.Name.rstrip(b"\x00"),
-        virt_addr=section.VirtualAddress,
-        virt_size=section.Misc_VirtualSize,
-        phys_size=section.SizeOfRawData,
-        md5=section.get_hash_md5(),
-        entropy=entr,
-        suspicious=(entr > 7 or entr < 3),
+    pe_section = ctypes.SectionPESection(
+        name = section.Name.rstrip(b'\x00'),
+        virt_addr = section.VirtualAddress,
+        virt_size = section.Misc_VirtualSize,
+        phys_size = section.SizeOfRawData,
+        md5 = section.get_hash_md5(),
+        entropy = entr,
+        suspicious = True if (entr>7 or entr<3) else False,
     )
+    return pe_section
 
 
 def analyze_file(raw_data: bytes) -> dict:
+
     pe = pefile.PE(data=raw_data)
 
     hashes = ctypes.SectionHash(
-        md5=hashlib.md5(raw_data).hexdigest(),
-        sha1=hashlib.sha1(raw_data).hexdigest(),
-        sha256=hashlib.sha256(raw_data).hexdigest(),
-        ssdeep=utils.ssdeep_hash(raw_data),
+        md5 = hashlib.md5(raw_data).hexdigest(),
+        sha1 = hashlib.sha1(raw_data).hexdigest(),
+        sha256 = hashlib.sha256(raw_data).hexdigest(),
+        ssdeep = utils.ssdeep(raw_data),
     )
 
     file_type = ctypes.SectionFileType(
-        python_magic=utils.from_magic(raw_data),
-        trid=utils.trid(raw_data),
+        python_magic = utils.from_magic(raw_data),
+        trid = utils.trid(raw_data),
     )
 
     meta = ctypes.SectionMeta(
-        size=len(raw_data),
-        architecture=utils.get_arch(pe.FILE_HEADER.Machine),
-        date=utils.get_timestamp(pe.FILE_HEADER.TimeDateStamp),
-        crc_claim=getattr(pe.OPTIONAL_HEADER, "CheckSum", "N/A"),
-        crc_calcd=pe.generate_checksum(),
+        size = len(raw_data),
+        architecture = utils.get_arch(pe.FILE_HEADER.Machine),
+        date = utils.get_timestamp(pe.FILE_HEADER.TimeDateStamp),
+        crc_claim = pe.OPTIONAL_HEADER.CheckSum,
+        crc_calcd = pe.generate_checksum(),
     )
 
     static_report = ctypes.StaticReport(
-        hashes=hashes,
-        file_type=file_type,
-        meta=meta,
-        pe_sections=[get_pe_section_info(section) for section in pe.sections],
+        hashes = hashes,
+        file_type = file_type,
+        meta = meta,
+        pe_sections = [get_pe_section_info(section) for section in pe.sections],
     )
 
     return static_report.model_dump()
