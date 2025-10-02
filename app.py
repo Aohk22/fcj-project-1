024d8b6b38073ed07cc4e162fbe8e22655707d47
phamquochuyphuc <pqhuyphuc2003@gmail.com>
2025-10-02 13:56:42 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+from fastapi import FastAPI, File, HTTPException
+from core.file_processor import analyze_file
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+from pefile import PEFormatError
+
+app = FastAPI()
+
+
+@app.post('/api/get_static/')
+async def file_upload(raw_data: Annotated[bytes, File()]):
+    try:
+        result = analyze_file(raw_data) 
+    except PEFormatError:
+        raise HTTPException(status_code=404, detail='File is not in the right format.')
+    return result
+
+
+
+frontend_path = Path(__file__).parent / "webserver_ec2"
+app.mount("/static", StaticFiles(directory=frontend_path), name="static")
+
+@app.get("/")
+async def serve_index():
+    index_file = frontend_path / "index.html"
+    if index_file.exists():
+        return FileResponse(index_file)
+    return {"error": "index.html not found"}
