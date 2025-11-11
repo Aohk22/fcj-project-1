02536d6e96b4dc087e6ed3b1f9922a155fa623bc
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-11-11 07:49:08 +0000

    update web lt userdata

--- terraform/envs/dev/user_data/web.sh
+++ terraform/envs/dev/user_data/web.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 apt update -y
-apt install -y git
+apt install -y git unzip
 curl -fsSL https://bun.sh/install | bash
-source ~/.bashrc
+source /root/.bashrc
+
 git clone https://github.com/Aohk22/fcj-project-1.git repo
 cd repo/
 git switch webserver-ec2
-cp app /app
+cp -r app /app
 
 cd /app
 nohup bun run index.ts >/var/log/http.log 2>&1 &
