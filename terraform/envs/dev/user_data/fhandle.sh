f1e5d928ad3a5f2705a04ea59b55a3b7d11a037e
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-11-11 10:00:16 +0000

    update userdata, pipeline components

--- terraform/envs/dev/user_data/fhandle.sh
+++ terraform/envs/dev/user_data/fhandle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd /app
+su ubuntu
+cd ~/app
 source .venv/bin/activate
 nohup fastapi run --port 4100 --host 0.0.0.0 >/var/log/http.log 2>&1 &
--- terraform/envs/dev/user_data/web.sh
+++ terraform/envs/dev/user_data/web.sh
@@ -3,14 +3,12 @@
 apt update -y
 apt install -y git unzip
 snap install bun-js
-# curl -fsSL https://bun.sh/install | bash
-source /root/.bashrc
 
-git clone https://github.com/Aohk22/fcj-project-1.git /app
-cd /app
-
+su ubuntu
+git clone https://github.com/Aohk22/fcj-project-1.git ~/app
+cd ~/app
 git switch webserver-ec2
 
-cd /app/app
+cd app
 
 nohup bun run index.ts >/var/log/http.log 2>&1 &
